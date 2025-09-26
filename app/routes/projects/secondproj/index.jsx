f617ced0ca63139fd4ea28032c5d66a83140479f
A2442 <a2442@MacBook-Pro-A2442.local>
2025-09-26 15:29:10 +0200

    added routing

---
+++ app/routes/projects/secondproj/index.jsx
@@ -0,0 +1,7 @@
+export async function loader() {
+  return null;
+}
+
+export default function SecondProjectsPage() {
+    return <h1>Second Project</h1>;
+}
---
+++ app/routes/projects/tradeapp/index.jsx
@@ -0,0 +1,7 @@
+export async function loader() {
+  return null;
+}
+
+export default function TradeAppPage() {
+    return <h1>Trade App</h1>;
+}
